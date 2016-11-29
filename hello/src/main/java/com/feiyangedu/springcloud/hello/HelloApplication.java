00bcecf7327b5d859b6eabcf9157a17420fe2056
Michael Liao <askxuefeng@gmail.com>
2016-11-29 16:13:06 +0800

    add hello spring-boot-app

--- hello/src/main/java/com/feiyangedu/springcloud/hello/HelloApplication.java
+++ hello/src/main/java/com/feiyangedu/springcloud/hello/HelloApplication.java
@@ -1,19 +1,31 @@
 package com.feiyangedu.springcloud.hello;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.Arrays;
+import java.util.List;
 
-@Controller
-@EnableAutoConfiguration
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@RestController
+@EnableSwagger2
 public class HelloApplication {
 
-	@RequestMapping("/")
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	@ResponseBody
 	String home() {
 		return "<h1>Hello World!</h1>";
+	}
+
+	@RequestMapping(value = "/api", method = RequestMethod.GET)
+	public List<String> api() {
+		return Arrays.asList("Hello", "Spring", "Boot");
 	}
 
 	public static void main(String[] args) throws Exception {
