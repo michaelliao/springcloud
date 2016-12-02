6703507ba080a57d13701fa1f32c70a946626d41
Michael Liao <askxuefeng@gmail.com>
2016-12-02 18:42:41 +0800

    add data-jdbc project

---
+++ data-jdbc/src/main/java/com/feiyangedu/springcloud/data/DataJdbcApplication.java
@@ -0,0 +1,98 @@
+package com.feiyangedu.springcloud.data;
+
+import java.util.List;
+import java.util.UUID;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.feiyangedu.springcloud.data.domain.User;
+import com.feiyangedu.springcloud.data.request.UserReq;
+
+/**
+ * Spring Boot Application using Spring Data Jpa.
+ * 
+ * @author Michael Liao
+ */
+@SpringBootApplication
+@RestController
+public class DataJdbcApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(DataJdbcApplication.class, args);
+	}
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	@PostConstruct
+	public void init() {
+		jdbcTemplate.execute("DROP TABLE IF EXISTS user");
+		jdbcTemplate.execute("CREATE TABLE user (" //
+				+ "id VARCHAR(50) NOT NULL," //
+				+ "email VARCHAR(50) NOT NULL," //
+				+ "name VARCHAR(50) NOT NULL," //
+				+ "gender BOOLEAN NOT NULL," //
+				+ "createdAt BIGINT NOT NULL," //
+				+ "updatedAt BIGINT NOT NULL," //
+				+ "PRIMARY KEY (id))");
+		for (int i = 0; i < 25; i++) {
+			UserReq req = new UserReq();
+			req.setEmail(randomString() + "@test.io");
+			req.setName("Mr " + randomString());
+			req.setGender(randomString().hashCode() % 2 == 0);
+			createUser(req);
+		}
+	}
+
+	@PostMapping("/users")
+	public User createUser(@RequestBody UserReq req) {
+		String id = randomString();
+		Long now = System.currentTimeMillis();
+		jdbcTemplate.update(
+				"INSERT INTO user (id, email, name, gender, createdAt, updatedAt) VALUES (?, ?, ?, ?, ?, ?)", // SQL
+				id, req.getEmail(), req.getName(), req.isGender(), now, now);
+		return getUser(id);
+	}
+
+	@GetMapping("/users")
+	public List<User> getUsers() {
+		return jdbcTemplate.query("SELECT * FROM user ORDER BY createdAt", new BeanPropertyRowMapper<>(User.class));
+	}
+
+	@GetMapping("/users/{id}")
+	public User getUser(@PathVariable("id") String id) {
+		return jdbcTemplate.queryForObject("SELECT * FROM user WHERE id=?", new BeanPropertyRowMapper<>(User.class),
+				id);
+	}
+
+	@PutMapping("/users/{id}")
+	public User updateUser(@PathVariable("id") String id, @RequestBody UserReq req) {
+		jdbcTemplate.update("UPDATE user SET email=?, name=?, gender=?, updatedAt=? WHERE id=?", // SQL
+				req.getEmail(), req.getName(), req.isGender(), System.currentTimeMillis(), id);
+		return getUser(id);
+	}
+
+	@DeleteMapping("/users/{id}")
+	public boolean deleteUser(@PathVariable("id") String id) {
+		int n = jdbcTemplate.update("DELETE FROM user WHERE id=?", id);
+		return n == 1;
+	}
+
+	private String randomString() {
+		return UUID.randomUUID().toString();
+	}
+
+}
