4400e5089f55f6c5d30d9197d1d00c897f1f827c
Michael Liao <askxuefeng@gmail.com>
2016-12-02 16:42:13 +0800

    add petstore-config project

---
+++ petstore-config/src/main/java/com/feiyangedu/springcloud/petstore/config/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.feiyangedu.springcloud.petstore.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
