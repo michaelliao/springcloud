908f0562548264b4a9079ab02a1a6e6b4a4ecf13
Michael Liao <askxuefeng@gmail.com>
2016-12-02 16:53:57 +0800

    add petstore-support project

---
+++ petstore-support/src/main/java/com/feiyangedu/springcloud/petstore/support/SupportApplication.java
@@ -0,0 +1,12 @@
+package com.feiyangedu.springcloud.petstore.support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportApplication.class, args);
+	}
+}
