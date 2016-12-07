3b385957809cd0e0cde588d49927f85cc25160ff
Michael Liao <askxuefeng@gmail.com>
2016-12-07 10:57:20 +0800

    add freemarker app

---
+++ freemarker/src/main/java/com/feiyangedu/springcloud/freemarker/mvc/User.java
@@ -0,0 +1,33 @@
+package com.feiyangedu.springcloud.freemarker.mvc;
+
+public class User {
+
+	private String id;
+	private String email;
+	private String name;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ freemarker/src/main/java/com/feiyangedu/springcloud/freemarker/mvc/UserController.java
@@ -0,0 +1,26 @@
+package com.feiyangedu.springcloud.freemarker.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UserController {
+
+	@GetMapping("/")
+	public ModelAndView index(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new ModelAndView("index", "name", name);
+	}
+
+	@GetMapping("/hello/{name}")
+	public ModelAndView hello(@PathVariable("name") String name) {
+		User user = new User();
+		user.setId("u" + Math.abs(name.hashCode()));
+		user.setName(name);
+		user.setEmail(name + " <" + name.toLowerCase() + "@example.com>");
+		return new ModelAndView("hello", "user", user);
+	}
+
+}
