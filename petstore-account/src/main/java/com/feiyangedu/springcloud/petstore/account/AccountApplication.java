83a88d6008839161d0de1dd6323676b57c7104fb
Michael Liao <askxuefeng@gmail.com>
2016-12-07 18:31:19 +0800

    simplify account config

--- petstore-account/src/main/java/com/feiyangedu/springcloud/petstore/account/AccountApplication.java
+++ petstore-account/src/main/java/com/feiyangedu/springcloud/petstore/account/AccountApplication.java
@@ -3,8 +3,10 @@ package com.feiyangedu.springcloud.petstore.account;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 @EnableDiscoveryClient
 public class AccountApplication {
 
