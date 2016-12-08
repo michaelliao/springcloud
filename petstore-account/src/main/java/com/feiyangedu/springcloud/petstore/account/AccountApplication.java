2262f4a4a1832f63660a27e5d46d71b510c6a71b
Michael Liao <askxuefeng@gmail.com>
2016-12-08 15:54:27 +0800

    update account

--- petstore-account/src/main/java/com/feiyangedu/springcloud/petstore/account/AccountApplication.java
+++ petstore-account/src/main/java/com/feiyangedu/springcloud/petstore/account/AccountApplication.java
@@ -3,11 +3,15 @@ package com.feiyangedu.springcloud.petstore.account;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.feiyangedu.springcloud.petstore.common.config.SwaggerConfig;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableDiscoveryClient
+@Import(SwaggerConfig.class)
 public class AccountApplication {
 
 	public static void main(String[] args) {
--- petstore-account/src/main/java/com/feiyangedu/springcloud/petstore/account/service/AccountService.java
+++ petstore-account/src/main/java/com/feiyangedu/springcloud/petstore/account/service/AccountService.java
@@ -1,8 +1,23 @@
 package com.feiyangedu.springcloud.petstore.account.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.feiyangedu.springcloud.petstore.account.domain.User;
+import com.feiyangedu.springcloud.petstore.account.repository.UserRepository;
+import com.feiyangedu.springcloud.petstore.common.service.AbstractService;
+
 @RestController
-public class AccountService {
+public class AccountService extends AbstractService {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@GetMapping("/api/users/{id}")
+	public User getUser(@PathVariable("id") String id) {
+		return checkNonNull(userRepository.findOne(id));
+	}
 
 }
