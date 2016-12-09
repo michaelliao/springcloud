f02f6c3130fbcfaa2eb81c7886ba1045710ae0e3
Michael Liao <askxuefeng@gmail.com>
2016-12-09 16:20:06 +0800

    add user bean

---
+++ petstore-account/src/main/java/com/feiyangedu/springcloud/petstore/account/bean/UserBean.java
@@ -0,0 +1,16 @@
+package com.feiyangedu.springcloud.petstore.account.bean;
+
+import java.time.LocalDate;
+
+import com.feiyangedu.springcloud.petstore.common.constant.Gender;
+
+public class UserBean {
+
+	public String email;
+
+	public String name;
+
+	public Gender gender;
+
+	public LocalDate birth;
+}
