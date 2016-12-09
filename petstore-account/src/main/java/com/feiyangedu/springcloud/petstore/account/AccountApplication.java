e4e18ae8d4de38cfa011313d0d8f417f2dbb40b6
Michael Liao <askxuefeng@gmail.com>
2016-12-09 18:34:07 +0800

    update account

--- petstore-account/src/main/java/com/feiyangedu/springcloud/petstore/account/AccountApplication.java
+++ petstore-account/src/main/java/com/feiyangedu/springcloud/petstore/account/AccountApplication.java
@@ -6,12 +6,12 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import com.feiyangedu.springcloud.petstore.common.config.SwaggerConfig;
+import com.feiyangedu.springcloud.petstore.common.config.CustomWebConfig;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableDiscoveryClient
-@Import(SwaggerConfig.class)
+@Import(CustomWebConfig.class)
 public class AccountApplication {
 
 	public static void main(String[] args) {
--- petstore-account/src/main/java/com/feiyangedu/springcloud/petstore/account/service/AccountService.java
+++ petstore-account/src/main/java/com/feiyangedu/springcloud/petstore/account/service/AccountService.java
@@ -1,13 +1,21 @@
 package com.feiyangedu.springcloud.petstore.account.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.feiyangedu.springcloud.petstore.account.bean.UserBean;
 import com.feiyangedu.springcloud.petstore.account.domain.User;
 import com.feiyangedu.springcloud.petstore.account.repository.UserRepository;
 import com.feiyangedu.springcloud.petstore.common.service.AbstractService;
+import com.feiyangedu.springcloud.petstore.common.util.BeanUtil;
 
 @RestController
 public class AccountService extends AbstractService {
@@ -15,9 +23,22 @@ public class AccountService extends AbstractService {
 	@Autowired
 	UserRepository userRepository;
 
+	@GetMapping("/api/users")
+	public Page<User> getUsers(@RequestParam(value = "page", defaultValue = "0") int pageIndex) {
+		return userRepository.findAll(new PageRequest(pageIndex, DEFAULT_PAGE_SIZE,
+				new Sort(new Sort.Order(Sort.Direction.DESC, "createdAt"))));
+	}
+
 	@GetMapping("/api/users/{id}")
 	public User getUser(@PathVariable("id") String id) {
 		return checkNonNull(userRepository.findOne(id));
 	}
 
+	@PostMapping("/api/users")
+	public User createUser(@RequestBody UserBean bean) {
+		User user = new User();
+		BeanUtil.copyPublicFields(bean, user);
+		userRepository.save(user);
+		return user;
+	}
 }
