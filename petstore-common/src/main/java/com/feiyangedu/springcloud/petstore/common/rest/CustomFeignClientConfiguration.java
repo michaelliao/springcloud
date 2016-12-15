c8863d0bd8f4ebe8afada97d4358343ce92fed10
Michael Liao <askxuefeng@gmail.com>
2016-12-15 16:06:01 +0800

    add log level in feign config

--- petstore-common/src/main/java/com/feiyangedu/springcloud/petstore/common/rest/CustomFeignClientConfiguration.java
+++ petstore-common/src/main/java/com/feiyangedu/springcloud/petstore/common/rest/CustomFeignClientConfiguration.java
@@ -1,16 +1,25 @@
 package com.feiyangedu.springcloud.petstore.common.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import feign.Logger;
 import feign.codec.ErrorDecoder;
 
 @Configuration
 public class CustomFeignClientConfiguration {
+
+	@Value("spring.feign.logger.level:BASIC")
+	Logger.Level loggerLevel;
 
 	@Bean
 	public ErrorDecoder feignErrorDecoder() {
 		return new RestErrorDecoder();
 	}
 
+	@Bean
+	Logger.Level feignLoggerLevel() {
+		return loggerLevel;
+	}
 }
