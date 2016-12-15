ca12783d01d17fb22e64d6d71e9dc2b81e44992a
Michael Liao <askxuefeng@gmail.com>
2016-12-15 15:52:41 +0800

    remove @EnableDiscoveryClient

--- petstore-account/src/main/java/com/feiyangedu/springcloud/petstore/account/AccountApplication.java
+++ petstore-account/src/main/java/com/feiyangedu/springcloud/petstore/account/AccountApplication.java
@@ -2,7 +2,6 @@ package com.feiyangedu.springcloud.petstore.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,7 +9,6 @@ import com.feiyangedu.springcloud.petstore.common.config.CustomWebConfig;
 
 @SpringBootApplication
 @EnableJpaRepositories
-@EnableDiscoveryClient
 @Import(CustomWebConfig.class)
 public class AccountApplication {
 
