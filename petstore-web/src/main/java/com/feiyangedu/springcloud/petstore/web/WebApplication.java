75d588d7a7ca77ec7e03c0f724db43cf7e5fba02
Michael Liao <askxuefeng@gmail.com>
2016-12-15 15:59:11 +0800

    update web app conf

--- petstore-web/src/main/java/com/feiyangedu/springcloud/petstore/web/WebApplication.java
+++ petstore-web/src/main/java/com/feiyangedu/springcloud/petstore/web/WebApplication.java
@@ -2,8 +2,12 @@ package com.feiyangedu.springcloud.petstore.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import com.feiyangedu.springcloud.petstore.common.config.CustomWebConfig;
 
 @SpringBootApplication
+@Import(CustomWebConfig.class)
 public class WebApplication {
 
 	public static void main(String[] args) {
