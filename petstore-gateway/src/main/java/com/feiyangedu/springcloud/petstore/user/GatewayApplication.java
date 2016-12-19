489ab1741fdd6980a87ef91e8de8338bb8f9e711
Michael Liao <askxuefeng@gmail.com>
2016-12-19 18:21:24 +0800

    rename gateway

---
+++ petstore-gateway/src/main/java/com/feiyangedu/springcloud/petstore/user/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.feiyangedu.springcloud.petstore.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+}
