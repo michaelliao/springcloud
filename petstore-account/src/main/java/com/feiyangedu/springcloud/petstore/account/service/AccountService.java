c5f218070716f6091c8383a4f981cf27f0b9c7c3
Michael Liao <askxuefeng@gmail.com>
2016-12-21 18:00:45 +0800

    refactor url

--- petstore-account/src/main/java/com/feiyangedu/springcloud/petstore/account/service/AccountService.java
+++ petstore-account/src/main/java/com/feiyangedu/springcloud/petstore/account/service/AccountService.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,7 @@ import com.feiyangedu.springcloud.petstore.common.service.AbstractService;
 import com.feiyangedu.springcloud.petstore.common.util.BeanUtil;
 
 @RestController
+@RequestMapping("/api/account")
 public class AccountService extends AbstractService {
 
 	@Autowired
@@ -28,26 +30,18 @@ public class AccountService extends AbstractService {
 	 * 
 	 * @param pageIndex
 	 */
-	@GetMapping("/api/users")
+	@GetMapping("/users")
 	public Page<User> getUsers(@RequestParam(value = "page", defaultValue = "0") int pageIndex) {
 		return userRepository.findAll(new PageRequest(pageIndex, DEFAULT_PAGE_SIZE,
 				new Sort(new Sort.Order(Sort.Direction.DESC, "createdAt"))));
 	}
 
-	@GetMapping("/api/users/{id}")
+	@GetMapping("/users/{id}")
 	public User getUser(@PathVariable("id") String id) {
 		return checkNonNull(userRepository.findOne(id));
 	}
 
-	@PostMapping("/api/users2")
-	public User createUser2(@RequestParam("aa") java.time.LocalDateTime aa, @RequestBody UserBean bean) {
-		User user = new User();
-		BeanUtil.copyPublicFields(bean, user);
-		userRepository.save(user);
-		return user;
-	}
-
-	@PostMapping("/api/users")
+	@PostMapping("/users")
 	public User createUser(@RequestBody UserBean bean) {
 		User user = new User();
 		BeanUtil.copyPublicFields(bean, user);
