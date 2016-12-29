3b70edaed5ce078e15bf7f618ed61c9770ecea60
Michael Liao <askxuefeng@gmail.com>
2016-12-29 18:09:45 +0800

    add security app

---
+++ security/src/main/java/com/feiyangedu/springcloud/security/SecureApplication.java
@@ -0,0 +1,32 @@
+package com.feiyangedu.springcloud.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Spring Boot Application.
+ * 
+ * @author Michael Liao
+ */
+@SpringBootApplication
+@RestController
+public class SecureApplication {
+
+	@GetMapping("/")
+	@ResponseBody
+	String home() {
+		return "<h1>Hello World!</h1>";
+	}
+
+	@GetMapping("/api")
+	public String[] api() {
+		return "Spring Boot Security".split(" ");
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SecureApplication.class, args);
+	}
+}
