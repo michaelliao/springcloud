a8c1c01ba22139046c1a18baaad47de4b2296b64
Michael Liao <askxuefeng@gmail.com>
2017-01-11 10:58:34 +0800

    fix gen-account-client.sh

---
+++ petstore-clients/gen-account-client.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+java -jar ../api-client-codegen/target/api-client-codegen-0.0.1-SNAPSHOT.jar --name=account --url=http://localhost:9001/v2/api-docs
+
--- petstore-clients/gen-account.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-java -jar ../api-client-codegen/target/api-client-codegen-0.0.1-SNAPSHOT.jar --name=account --url=http://localhost:9002/v2/api-docs
-
